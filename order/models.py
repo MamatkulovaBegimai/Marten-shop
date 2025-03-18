fc4c60a8ad4a4fccb89334d66ee72d75d5fa3c10
begimai <begimajmamatkulova7@gmail.com>
2025-03-18 14:16:18 +0600

    second commit

--- order/models.py
+++ order/models.py
@@ -38,3 +38,46 @@ class BillingInformation(models.Model):
 
     def __str__(self):
         return f"Billing Info for {self.user.username}"
+
+
+class ShippingInformation(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name="shipping_information")
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    use_billing_information = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Shipping Info for {self.user.username}"
+
+class ShippingMethod(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - {self.price} сом"
+
+class PaymentInformation(models.Model):
+    METHOD_CHOICES = [
+        ('money_order', 'Check/Money Order'),
+        ('credit_card', 'Credit Card'),
+    ]
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="payment")
+    method = models.CharField(max_length=255, choices=METHOD_CHOICES, default="money_order")
+
+class CreditCard(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="credit_card")
+    card_name = models.CharField(max_length=100)
+    card_type = models.CharField(max_length=20, choices=[
+        ('visa', 'Visa'),
+        ('mastercard', 'MasterCard'),
+    ])
+    card_number = models.CharField(max_length=16)
+    expiration_month = models.PositiveSmallIntegerField()
+    expiration_year = models.PositiveSmallIntegerField()
+    cvv = models.CharField(max_length=3)
+
+    def __str__(self):
+        return f"{self.card_type} - {self.user.username}"
+
+
+
